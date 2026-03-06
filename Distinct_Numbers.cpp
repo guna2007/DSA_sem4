fb5549abb1a1b6c3788035ccc31699d3cbe2e0f4
guna2007 <lankalapalligunapersonal@gmail.com>
2026-03-06 23:37:15 +0530

    6th mar

---
+++ Distinct_Numbers.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    set<int> s;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        s.insert(x);
+    }
+    cout << s.size() << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
